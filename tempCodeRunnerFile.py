47416840f32be46a8da4aa89507816e21a960ed2
Ivana Chipeja <ivanaisabelchipeja@gmail.com>
2024-06-09 16:38:05 +0200

    updated flowchart

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
- print("\t\t\t******     1. Add Books               ******")
-    print("\t\t\t******     2. Remove Books            ******")
-    print("\t\t\t******     3. List Books              ******")
-    print("\t\t\t******     4. See Bookstore Profits   ******")
